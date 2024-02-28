800f6f53ded4b85ec964dd7eecdb5864362537e8
christiansaccani <christian.saccani99@gmail.com>
2024-02-28 15:22:44 +0100

    Exercise done!

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,7 +1,21 @@
 <script >
     export default {
         name: "AppHero",
-    }
+
+        data() {
+            return {
+                message: "",
+            }
+        },
+
+        methods: {
+            buttonTrigger() {
+                this.message = "Absolutely Nothing is happening!";
+            }
+        },
+    };
+
+    
 </script>
 
 <template>
@@ -11,6 +25,15 @@
             <h2>
                 A Friendly Purple Jumbotron
             </h2>
+            <p>
+                Jumbotron it's a really big and funny section. Like a very big board! <br>
+                You can also add a button to make your Friendly Purple Jumbotron <br>
+                more friendly!
+            </p>
+            <button @click="buttonTrigger()">
+                Click me!
+            </button>
+            {{ message }}
         </div>
     </div>
 
@@ -20,15 +43,36 @@
     #jumbotron-container {
         margin-top: 1em;
         border: 1px solid white;
+
+        display: flex;
+        justify-content: center;
+
+        background-color: rgba(137, 43, 226, 0.459);
     }
 
     #jumbotron {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
         text-align: center;
         margin: 1em;
 
         border: 1px solid white;
-        padding: 1em;
+        padding: 2em 2em;
 
         background-color: blueviolet;
+    }
+
+    button {
+        color: blueviolet;
+        background-color: whitesmoke;
+
+        border: none;
+        border-radius: 12px;
+        margin: 1em 0em;
+        padding: 1em;
+
+        cursor: pointer;
     }
 </style>
