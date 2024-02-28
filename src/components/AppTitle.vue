2df68012c55c3a94339d754b1ae799f770bb637c
christiansaccani <christian.saccani99@gmail.com>
2024-02-28 14:49:45 +0100

    hello world done

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script >
+    export default {
+        name: "Title",
+    }
+</script>
+
+<template>
+    <h1>
+        La mia prima app con Vite!
+    </h1>
+</template>
+
+<style >
+    h1 {
+        text-align: center;
+    }
+</style>
